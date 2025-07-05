712a519e066ebef36cfb26a2a7638d96af9c9c6a
Antoine <antoine.ribeyre@epitech.eu>
2025-07-05 16:40:53 +0200

    feat(Dialog-Recip-V1)

--- app/src/main/java/com/pdfa/pdfa_app/user_interface/screens/RecipeScreen_Maker.kt
+++ app/src/main/java/com/pdfa/pdfa_app/user_interface/screens/RecipeScreen_Maker.kt
@@ -1,12 +1,10 @@
 package com.pdfa.pdfa_app.user_interface.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,29 +12,29 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.pdfa.pdfa_app.ui.theme.AppColors
 import com.pdfa.pdfa_app.ui.theme.AppShapes
 import com.pdfa.pdfa_app.ui.theme.AppSpacing
 import com.pdfa.pdfa_app.user_interface.component.RecipeCard
+import com.pdfa.pdfa_app.user_interface.component.RecipeParameter
 import com.pdfa.pdfa_app.user_interface.component.ScrollbarPersonnalisee
-import com.pdfa.pdfa_app.user_interface.component.TagsBox
 
 @Composable
 fun RecipeMakerScreen(){
 
     val scrollState = rememberScrollState()
+    var showDialog by remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
@@ -76,15 +74,17 @@ fun RecipeMakerScreen(){
                     .background(AppColors.Primary)
                     .padding(bottom = AppSpacing.L, start = AppSpacing.L, end = AppSpacing.L),
 
-            ){
+                ){
                 Box(
                     contentAlignment = Alignment.Center,
                     modifier = Modifier
                         .fillMaxSize()
                         .clip(AppShapes.CornerL)
-                        .background(AppColors.MainGreen),
-
-                    ) {
+                        .background(AppColors.MainGreen)
+                        .clickable {
+                            showDialog = true
+                        }
+                ) {
                     Text(
                         text = "Plus de recette"
                     )
@@ -93,4 +93,7 @@ fun RecipeMakerScreen(){
         }
     }
 
+    if (showDialog) {
+        RecipeParameter(onDismiss = { showDialog = false })
+    }
 }
