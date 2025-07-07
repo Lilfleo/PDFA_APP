e5395a68c650f9a767911642005f4f3605bfdd10
Paul Decauchy <paul.decauchy@epitech.eu>
2025-07-07 15:25:57 +0200

    feat(data): added update and insert method in food detail repository

--- app/src/main/java/com/pdfa/pdfa_app/data/dao/FoodDetailDao.kt
+++ app/src/main/java/com/pdfa/pdfa_app/data/dao/FoodDetailDao.kt
@@ -4,8 +4,8 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Transaction
-import com.pdfa.pdfa_app.data.model.Allergy
-import com.pdfa.pdfa_app.data.model.AllergyWithFood
+import androidx.room.Update
+import androidx.room.Upsert
 import com.pdfa.pdfa_app.data.model.FoodDetail
 import com.pdfa.pdfa_app.data.model.FoodDetailWithFood
 import kotlinx.coroutines.flow.Flow
@@ -23,6 +23,10 @@ interface FoodDetailDao {
     @Query("SELECT * FROM food_detail WHERE food_id = :foodId")
     fun getFoodDetail(foodId: Int): Flow<FoodDetailWithFood>
 
+    @Update
+    suspend fun updateFoodDetail(foodDetail: FoodDetail)
 
+    @Upsert
+    suspend fun upsertFoodDetail(foodDetail: FoodDetail): Long
 }
 
--- app/src/main/java/com/pdfa/pdfa_app/data/repository/FoodDetailRepository.kt
+++ app/src/main/java/com/pdfa/pdfa_app/data/repository/FoodDetailRepository.kt
@@ -1,6 +1,7 @@
 package com.pdfa.pdfa_app.data.repository
 
 import com.pdfa.pdfa_app.data.dao.FoodDetailDao
+import com.pdfa.pdfa_app.data.model.Food
 import com.pdfa.pdfa_app.data.model.FoodDetail
 import com.pdfa.pdfa_app.data.model.FoodDetailWithFood
 import kotlinx.coroutines.flow.Flow
@@ -18,4 +19,12 @@ class FoodDetailRepository @Inject constructor(
     suspend fun insert(allergy: FoodDetail) {
         dao.insertFoodDetail(allergy)
     }
+
+    suspend fun update(foodDetail: FoodDetail) {
+        dao.updateFoodDetail(foodDetail)
+    }
+
+    // Upsert inserts the FoodDetail if its ID is 0 (i.e. new), or updates the existing row if the ID matches an existing entry.
+    // Returns the inserted or updated row ID.
+    suspend fun upsert(foodDetail: FoodDetail): Long = dao.upsertFoodDetail(foodDetail)
 }
