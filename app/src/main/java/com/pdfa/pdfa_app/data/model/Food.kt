2e7d34b4490c08626dc875a5467bed3bcc1f8e95
Paul Decauchy <paul.decauchy@epitech.eu>
2025-08-04 16:45:50 +0200

    feat(data): seed working for utensils and food

--- app/src/main/java/com/pdfa/pdfa_app/data/model/Food.kt
+++ app/src/main/java/com/pdfa/pdfa_app/data/model/Food.kt
@@ -4,6 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
+import java.util.Calendar
 import java.util.Date
 
 @Serializable
@@ -14,5 +15,9 @@ data class Food(
     val link: String,
     val caloriesPerKg: Int,
     val caloriesPerUnit: Int,
-    @Contextual val expirationTime: Date
+    @Contextual val expirationTime: Date = run {
+        Calendar.getInstance().apply {
+            add(Calendar.DAY_OF_MONTH, 7)
+        }.time
+    }
 )
